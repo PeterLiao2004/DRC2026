8f2139b3eb27492745944b01de40128ebc2ef6cf
SofiaLim <sofia12lim@gmail.com>
2026-06-25 01:58:42 +1000

    Serial test for speed

--- pi_src/Serial_test.py
+++ pi_src/Serial_test.py
@@ -7,13 +7,13 @@ BAUD = 115200
 ser = serial.Serial(PORT, BAUD, timeout=1)
 time.sleep(2)
 
-numbers = [-100, -75, -10, -8, -2, 0, 2, 5, 7, 10, 30, 50, 75, 100]
+numbers = [2, 5, 7, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
 
 for n in numbers:
-    msg = f"{n}\n"
+    msg = f"D,0,{n}\n"
     ser.write(msg.encode("utf-8"))
     print(f"Sent: {n}")
 
-    time.sleep(2)
+    time.sleep(1)
 
 ser.close()
